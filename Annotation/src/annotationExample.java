7a9ed85827a1d4ce3264982d3c5404f7b9a68095
Mayur Suresh Shelke <shelke.mayur@gmail.com>
2022-01-13 19:26:59 +0530

    Annotation Added

---
+++ Annotation/src/annotationExample.java
@@ -0,0 +1,13 @@
+
+public class annotationExample {
+	
+	@customAnnotation(salary= 10000 , roll="Develper")
+	public void ankitInfo() {
+		
+		System.out.println("applied annotation");
+		
+	}
+	
+	
+
+}
